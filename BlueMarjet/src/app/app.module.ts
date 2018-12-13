937fa0d93dd4f001852cffd77893f2c8cde0f0ad
DaniloMerconSantos <danilo.mercon@fcamara.com.br>
2018-12-13 11:29:12 -0200

    Criação dos componentes.

--- BlueMarjet/src/app/app.module.ts
+++ BlueMarjet/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { EnderecoComponent } from './endereco/endereco.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    EnderecoComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
